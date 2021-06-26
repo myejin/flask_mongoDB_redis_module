42101e2c3cf0ad47d60f2c9d5922f133332b2e66
myejin <rlagpwlsq78@gmail.com>
2021-06-26 14:48:50 +0000

    MongoClient 삭제 및 시크릿키 추가
    MongoClient 는 다른 클래스 또는 함수에 추가하였음. 어떻게 구성할지 고민이 필요함.
    wtforms 사용에 필요한 임의의 시크릿 키를 추가하였음.

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from pymongo import MongoClient
-from flask import Flask, redirect
+from flask import Flask, redirect, jsonify
 from SignupView import Signup
-
 app = Flask(__name__)
-conn = MongoClient('localhost')
 
 app.add_url_rule('/signup/', view_func = Signup.as_view('signup'))
 
 @app.route('/')
 def root():
     return redirect('/signup/')
-
+    
 if __name__ == '__main__':
+    app.config['SECRET_KEY'] = b'\xc2\xf8{\xe0I\xf3kV\xd2Y\x90uF\xbeRL'
     app.run(debug = True, host='0.0.0.0') # 운영서버에서는 디버그모드 비활성화
-    conn.close()
