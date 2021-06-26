c1db3ecfd438d988dd1bf2a8d81af40aba61f4fe
myejin <rlagpwlsq78@gmail.com>
2021-06-26 05:45:14 +0000

    flask와 mongodb 설치
    부팅 후 mongod가 enable 된다.

---
+++ settings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+sudo echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org python3 python3-pip 
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo pip3 install pymongo flask
