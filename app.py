68bc1f8f721c3a1f21709fd080b8ba6439a8dbf5
myejin <rlagpwlsq78@gmail.com>
2021-06-27 08:34:51 +0000

    홈 리다이렉트 추가
    기존 /signup 에서 home.html 로 이동

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, redirect, jsonify
-from SignupView import Signup
+from flask import Flask, render_template, jsonify
+from SignupView import Signup, Login
+#from flask_bcrypt import Bcrypt 
 app = Flask(__name__)
+#bcrypt = Bcrypt(app)
 
 app.add_url_rule('/signup/', view_func = Signup.as_view('signup'))
+app.add_url_rule('/login/', view_func = Login.as_view('login'))
 
 @app.route('/')
 def root():
-    return redirect('/signup/')
+    return render_template('home.html') 
     
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = b'\xc2\xf8{\xe0I\xf3kV\xd2Y\x90uF\xbeRL'
