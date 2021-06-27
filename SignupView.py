b0704a880c8202cd1cb5a6f4e9f01bdcb5334344
myejin <rlagpwlsq78@gmail.com>
2021-06-27 08:39:47 +0000

    로그인 클래스 생성
    추후, bcrypt 적용 시 변경해야함

--- SignupView.py
+++ SignupView.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-from flask import render_template, flash 
+from flask import render_template, redirect, flash, session
 from flask.views import MethodView
-from forms import Signup_Form
+from forms import Signup_Form, Login_Form
 
 class Signup(MethodView):
     def post(self):
         form = Signup_Form()
-        if form.validate_on_submit() == False:
+        if form.validate_on_submit() == False:            
             for msg in form.errors.values():
                 if msg:
                     flash(str(msg[0]))
@@ -14,6 +14,7 @@ class Signup(MethodView):
 
         userid = form.data.get('userid')
         password = form.data.get('password')
+        # 해쉬
         doc = {
             'id':userid,
             'pw':password
@@ -23,7 +24,23 @@ class Signup(MethodView):
         # 에러핸들러
         flash('회원가입 성공!') 
         conn.close()
-        return render_template('signup.html', form = form)  # login 페이지로 교체 
+        return redirect('/')
         
     def get(self):
         return render_template('signup.html', form = Signup_Form()) 
+
+class Login(MethodView):
+    def post(self):
+        form = Login_Form()
+        if form.validate_on_submit() == False:            
+            for msg in form.errors.values():
+                if msg:
+                    flash(str(msg[0]))
+                    return render_template('login.html', form = form) 
+
+        session['userid'] = form.data.get('userid')
+        flash('로그인 되었습니다!')
+        return redirect('/')
+        
+    def get(self):
+        return render_template('login.html', form = Login_Form()) 
