1289d56f33ce8c0a2a21135c7c3518e9ec2bad8b
myejin <rlagpwlsq78@gmail.com>
2021-06-27 16:26:29 +0000

    레디스 연동 추가
    Flask-Session 활용해 서버측 세션관리 구현, timeout = 10분
    참고 1) https://testdriven.io/blog/flask-server-side-sessions/
         2) https://flask-session.readthedocs.io/en/latest/

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, session
 from SignupView import Signup, Login, Logout
-#from flask_bcrypt import Bcrypt 
-app = Flask(__name__)
-#bcrypt = Bcrypt(app)
+from flask_session import Session
+import redis
+from datetime import timedelta
 
+#from flask_bcrypt import Bcrypt
+#bcrypt = Bcrypt(app) 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = b'\xc2\xf8{\xe0I\xf3kV\xd2Y\x90uF\xbeRL'
+app.config['SESSION_TYPE'] = 'redis'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 10)
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = redis.from_url('redis://localhost:6379')
+
+server_session = Session(app)
 
 app.add_url_rule('/signup/', view_func = Signup.as_view('signup'))
 app.add_url_rule('/login/', view_func = Login.as_view('login'))
@@ -13,7 +24,7 @@ app.add_url_rule('/logout/', view_func = Logout.as_view('logout'))
 def home():
     userid = session.get('userid', None)
     return render_template('home.html', userid=userid) 
-    
+
+
 if __name__ == '__main__':
-    app.config['SECRET_KEY'] = b'\xc2\xf8{\xe0I\xf3kV\xd2Y\x90uF\xbeRL'
     app.run(debug = True, host='0.0.0.0') # 운영서버에서는 디버그모드 비활성화
