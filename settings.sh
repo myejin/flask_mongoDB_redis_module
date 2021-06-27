0bf2fcfab8ec37e57e36a5a60d69e4e20558033a
myejin <rlagpwlsq78@gmail.com>
2021-06-27 16:25:35 +0000

    모듈 추가
    Flask-Session, 서버 측 세션 관리를 위해 추가함.

--- settings.sh
+++ settings.sh
@@ -2,7 +2,8 @@
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 sudo echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org python3 python3-pip 
+sudo apt-get install -y mongodb-org redis-server python3 python3-pip 
 sudo systemctl start mongod
 sudo systemctl enable mongod
-sudo pip3 install pymongo flask Flask-WTF Flask-Bcrypt
+sudo pip3 install pymongo redis flask Flask-WTF Flask-Bcrypt Flask-Session
+sudo service redis-server restart
